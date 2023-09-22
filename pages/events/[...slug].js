8ef17dff892850a5c1cca351d6e45cc5057af3a3
Gabriela Monaro <gabriela.monaro@outlook.com>
2023-09-22 14:44:10 -0300

    feat: cards creating

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilteredEventesPage() {
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
+}
+
+export default FilteredEventesPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,10 @@
+function EventDetail() {
+    return (
+      <div>
+        <h1>The Home Page</h1>
+      </div>
+    );
+  }
+  
+  export default EventDetail;
+  
